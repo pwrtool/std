bd95d56684c4edb4237e45525717269ec2658d5b
firesquid <jdeiss06@gmail.com>
2023-09-15 19:20:06 -0500

    add example kit

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
-export function sayHello() {
-  console.log("Hello World");
-}
+import { io, powertool, CliArgs } from "@pwrtool/kit";
+
+powertool.tool("hello", "says hello", async () => {
+  io.out("Hello World!");
+});
+
+powertool.tool("io-test", "Testing io", async () => {
+  const answer = await io.dichotomous("Are you sure you want to continue?");
+  console.log(answer);
+
+  const answer2 = await io.prompt<string>("What is your name?");
+  console.log(answer2);
+});
+
+powertool.tool("cli-test", "testing cli args", async () => {
+  const args = new CliArgs();
+
+  console.log(args.get("name"));
+});
+
+powertool.run();
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 cd "$(dirname "$0")"
 
 # Run the index.ts file
-bun run ./index.ts
+bun run ./index.ts $1 $2 $3 $4 $5 $6 $7 $8 $9
