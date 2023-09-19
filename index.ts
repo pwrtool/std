26576fbf859c6c6fce07d55d3e70cec20ecc8402
firesquid <jdeiss06@gmail.com>
2023-09-18 21:34:21 -0500

    add template tool

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { io, powertool, CliArgs, exitWithSuccess } from "@pwrtool/kit";
+import { join } from "node:path";
+import fs from "node:fs";
 
+// DEMOS
 powertool.tool("hello", "says hello", async () => {
   io.out("Hello World!");
 });
-
-powertool.tool("io-test", "Testing io", async () => {
+powertool.tool("io-demo", "Demoing the io", async () => {
   const answer = await io.dichotomous("Are you sure you want to continue?");
   console.log(answer);
 
@@ -13,13 +15,45 @@ powertool.tool("io-test", "Testing io", async () => {
 
   exitWithSuccess();
 });
-
-powertool.tool("cli-test", "testing cli args", async () => {
+powertool.tool("cli-demo", "demoing cli args", () => {
   const args = new CliArgs();
 
   console.log(args.get("name"));
 
   exitWithSuccess();
 });
+powertool.tool(
+  "cwd-demo",
+  "demos getting the cwd from the cli args object",
+  () => {
+    const args = new CliArgs();
+    const cwd = args.get("calldir");
 
+    io.out(`Current working directory: ${cwd}`);
+    exitWithSuccess();
+  }
+);
+
+// TEMPLATES
+powertool.tool(
+  "template-tool",
+  "copies all the necessary files for creating a tool",
+  async () => {
+    const args = new CliArgs();
+    const cwd = args.get<string>("calldir");
+
+    const path = join(
+      cwd,
+      await io.prompt("Where wold you like to create the tool?\n")
+    );
+
+    io.out(path);
+
+    fs.existsSync(path) || fs.mkdirSync(path, { recursive: true });
+
+    exitWithSuccess();
+  }
+);
+
+// RUN THE FILE
 powertool.run();
